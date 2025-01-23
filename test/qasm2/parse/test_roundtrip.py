6a802cbf0d407a79541764cae0b6145436a9ecf4
Roger-luo <rogerluo.rl18@gmail.com>
2025-01-23 12:32:01 -0500

    adopt some test examples

--- test/qasm2/parse/test_roundtrip.py
+++ test/qasm2/parse/test_roundtrip.py
@@ -5,7 +5,7 @@ from bloqade.qasm2.parse import loads, pprint, loadfile
 
 
 def roundtrip(file):
-    ast1 = loadfile(os.path.join(os.path.dirname(__file__), file))
+    ast1 = loadfile(os.path.join(os.path.dirname(__file__), "programs", file))
     buf = io.StringIO()
     pprint(ast1, buf)
     ast2 = loads(buf.getvalue())
