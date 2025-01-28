475a7a3b7461eb60f69a10f3ab72fea3ef351281
Roger-luo <rogerluo.rl18@gmail.com>
2025-01-28 14:23:26 -0500

    fix a few issues

--- src/bloqade/runtime/qrack/__init__.py
+++ src/bloqade/runtime/qrack/__init__.py
@@ -1,5 +1,5 @@
-from .run import pyqrack_execute as pyqrack_execute
 from .base import Memory as Memory, PyQrackInterpreter as PyQrackInterpreter
 
 # NOTE: The following import is for registering the method tables
 from .qasm2 import uop as uop, core as core, parallel as parallel
+from .target import PyQrack as PyQrack
