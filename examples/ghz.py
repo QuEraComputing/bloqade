f481471f908659d808f73d3cf1df6efe577c47af
kaihsin <kaihsinwu@gmail.com>
2025-02-06 22:49:04 -0500

    refactor ghz, add docs in exampple

--- examples/ghz.py
+++ examples/ghz.py
@@ -4,7 +4,9 @@ from bloqade import qasm2
 from kirin.dialects import ilist
 
 
-# simple linear depth impl of ghz state prep
+# > Simple linear depth impl of ghz state prep
+# A simple GHZ state preparation circuit can be done with N CX gates and 1 H gate.
+# This gives circuit execution depth of N+1.
 def ghz_linear(n_qubits: int):
 
     @qasm2.main
@@ -18,7 +20,15 @@ def ghz_linear(n_qubits: int):
     return ghz_linear_program
 
 
-# 1/2 linear depth by re-arranging
+# > 1/2 linear depth by re-arranging
+# Let's take a look how we can rewrite the circuit toward more QuEra's hardware friendly circuit.
+# If we put the first haradamard gate on the middle qubit, and fan out the CX gate,
+# we can effectively reduce the circuit depth to N/2 + 1. With each layer have two CX gates.
+
+
+# Note it is important to separate the concept of circuit depth and circuit execution depth.
+# For example, in the following implementation, the two CX gates instruction inside the for loop are executed in sequence.
+# So even thought the circuit depth is N/2 + 1. The circuit execution depth is still N + 1.
 def ghz_half(n_qubits: int):
     @qasm2.main
     def ghz_half_program():
@@ -40,7 +50,23 @@ def ghz_half(n_qubits: int):
     return ghz_half_program
 
 
-# 1/2 linear depth by re-arranging, and using parallelism
+# > 1/2 linear depth by re-arranging, and using parallelism
+# Now lets see how we can ultilize QuEra's neutral atom's unique parallel feature to reduce the
+# actual execution time of the circuit.
+# It is important to know that on our digital quantum computer, by nature can execute native gate in parallel in an single instruction/ execution cycle.
+# The concept is very similar to the SIMD (Single Instruction, Multiple Data) in classical computing.
+# On our hardware, the native gate set is arbitrary (parallel) rotations and (parallel) CZ gates.
+#
+# We know that:
+# 1. hadamard can be decomposed into a Ry(pi/2) rotation follow by a X gate.
+# 2. the CX gate can be decomposed into CZ gate with two hadamards acting on the target qubit.
+# 3. CZ gate commute with each other CZ gates.
+# Base on the above observation, one can rewrite the circuit with only CZ gates in the middle,
+# and some left-over parallel hadamard gates at the beginning and the end of the circuit
+
+
+# We can now further rewrite the above circuit to using parallel gates.
+# using parallel gates, we can further reduce the circuit execution depth to 6 (constant and does not scale with N)
 def ghz_half_simd(n_qubits: int):
     @qasm2.main
     def ghz_half_simd_program():
@@ -54,6 +80,7 @@ def ghz_half_simd(n_qubits: int):
             return qreg[i]
 
         even_qubits = ilist.Map(fn=get_qubit, collection=range(0, n_qubits, 2))
+        odd_qubits = ilist.Map(fn=get_qubit, collection=range(1, n_qubits, 2))
 
         # acting parallel H = XRy^{pi/2} on even qubits and middle qubit
         initial_targets = even_qubits + [qreg[s]]
@@ -62,14 +89,9 @@ def ghz_half_simd(n_qubits: int):
         # X
         qasm2.parallel.u(qargs=initial_targets, theta=math.pi, phi=0.0, lam=math.pi)
 
-        # fan out the CZ gate:
-        qasm2.cz(qreg[s], qreg[s - 1])
-
-        for i in range(s - 1, 0, -1):
-            qasm2.parallel.cz(
-                ctrls=[qreg[i], qreg[n_qubits - i - 1]],
-                qargs=[qreg[i - 1], qreg[n_qubits - i]],
-            )
+        # two layer of parallel CZ gates
+        qasm2.parallel.cz(ctrls=even_qubits, qargs=odd_qubits)
+        qasm2.parallel.cz(ctrls=odd_qubits[:-1], qargs=even_qubits[1:])
 
         # acting parallel H = Ry^{-pi/2}X on even qubits only:
         # Ry(pi/2)
@@ -80,7 +102,7 @@ def ghz_half_simd(n_qubits: int):
     return ghz_half_simd_program
 
 
-# Note:
+# Note on using closure to capture global variable:
 # Since qasm2 does not allow main program with arguments, so we need to put the program in a closure.
 # our kirin compiler toolchain can capture the global variable inside the closure.
 # In this case, the n_qubits will be captured upon calling the `ghz_half_simd(n_qubits)` python function,
