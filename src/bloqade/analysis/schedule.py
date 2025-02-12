0520ed7be90cbd06e339881f15ea43e6ef8656b8
Phillip Weinberg <weinbe58@gmail.com>
2025-02-12 15:57:29 -0500

    sketching rewrite rule

--- src/bloqade/analysis/schedule.py
+++ src/bloqade/analysis/schedule.py
@@ -1,4 +1,4 @@
-from typing import Any, Set, Dict, List, Callable, Iterable, Optional, final
+from typing import Any, Set, Dict, Iterable, Optional, final
 from itertools import chain
 from dataclasses import field, dataclass
 from collections.abc import Sequence
@@ -85,10 +85,10 @@ class StmtDag(graph.Graph[ir.Statement]):
             for node_id in self.out_edges.get(self.nodes[node], set())
         )
 
-    def get_neighbors(self, node: ir.Statement) -> graph.Iterable[ir.Statement]:
+    def get_neighbors(self, node: ir.Statement) -> Iterable[ir.Statement]:
         return chain(self.get_parents(node), self.get_children(node))
 
-    def get_nodes(self) -> graph.Iterable[ir.Statement]:
+    def get_nodes(self) -> Iterable[ir.Statement]:
         return self.stmts.values()
 
     def get_edges(self) -> Iterable[tuple[ir.Statement, ir.Statement]]:
@@ -114,23 +114,27 @@ class DagScheduleAnalysis(Forward[GateSchedule]):
     address_analysis: Dict[ir.SSAValue, address.Address]
     use_def: Dict[int, ir.Statement] = field(init=False)
     stmt_dag: StmtDag = field(init=False)
-    stmt_dags: List[StmtDag] = field(init=False)
+    stmt_dags: Dict[ir.Block, StmtDag] = field(init=False)
 
     def initialize(self):
         super().initialize()
         self.use_def = {}
         self.stmt_dag = StmtDag()
-        self.stmt_dags = []
+        self.stmt_dags = {}
 
-    def push_current_dag(self):
+    def push_current_dag(self, block: ir.Block):
         # run when hitting terminator statements
-        self.stmt_dags.append(self.stmt_dag)
+        assert block not in self.stmt_dags, "Block already in stmt_dags"
+        self.stmt_dags[block] = self.stmt_dag
         self.stmt_dag = StmtDag()
         self.use_def = {}
 
     def eval_stmt_fallback(self, frame: ForwardFrame, stmt: ir.Statement):
         if stmt.has_trait(ir.IsTerminator):
-            self.push_current_dag()
+            assert (
+                stmt.parent_block is not None
+            ), "Terminator statement has no parent block"
+            self.push_current_dag(stmt.parent_block)
 
         return tuple(self.lattice.top() for _ in stmt.results)
 
@@ -170,116 +174,3 @@ class DagScheduleAnalysis(Forward[GateSchedule]):
 
         self.run(mt, args, kwargs).expect()
         return self.stmt_dags
-
-
-def get_topological_groups(dag: StmtDag):
-
-    groups: List[List[str]] = []
-    inc_edges = {k: set(v) for k, v in dag.inc_edges.items()}
-
-    while inc_edges:
-        # get edges with no dependencies
-        group = [node_id for node_id, inc_edges in inc_edges.items() if not inc_edges]
-
-        groups.append(group)
-        # remove nodes in group from inc_edges
-        for n in group:
-            inc_edges.pop(n)
-            for m in dag.out_edges[n]:
-                inc_edges[m].remove(n)
-
-    return groups
-
-
-def same_id_checker(ssa1: ir.SSAValue, ssa2: ir.SSAValue):
-    return ssa1 is ssa2
-
-
-@dataclass
-class CanMerge:
-    same_id_checker: Callable[[ir.SSAValue, ir.SSAValue], bool]
-
-    def check_equiv_args(
-        self,
-        args1: Iterable[ir.SSAValue],
-        args2: Iterable[ir.SSAValue],
-    ):
-        try:
-            return all(
-                self.same_id_checker(ssa1, ssa2)
-                for ssa1, ssa2 in zip(args1, args2, strict=True)
-            )
-        except ValueError:
-            return False
-
-    def __call__(self, stmt1: ir.Statement, stmt2: ir.Statement):
-        from bloqade.qasm2.dialects import uop, parallel
-
-        match stmt1, stmt2:
-            case (
-                (parallel.UGate(), parallel.UGate())
-                | (parallel.UGate(), uop.UGate())
-                | (uop.UGate(), parallel.UGate())
-                | (uop.UGate(), parallel.UGate())
-                | (uop.UGate(), parallel.UGate())
-                | (parallel.RZ(), parallel.RZ())
-            ):
-                return self.check_equiv_args(stmt1.args[1:], stmt2.args[1:])
-            case (
-                (parallel.CZ(), parallel.CZ())
-                | (parallel.CZ, uop.CZ)
-                | (uop.CZ, parallel.CZ)
-            ):
-                return True
-
-            case _:
-                return False
-
-
-@dataclass
-class GreedyMergeGates:
-    can_merge: Callable[[ir.Statement, ir.Statement], bool]
-
-    def __call__(
-        self,
-        gates: List[ir.Statement],
-    ) -> List[List[ir.Statement]]:
-
-        groups = []
-
-        for gate in gates:
-            grouped = False
-            for group in groups:
-                if any(self.can_merge(gate, group_gate) for group_gate in group):
-                    group.append(gate)
-                    grouped = True
-                    break
-
-            if not grouped:
-                groups.append([gate])
-
-        return groups
-
-
-def parallel_grouping(
-    dag: StmtDag,
-    merge_gates: Callable[
-        [List[ir.Statement]], List[List[ir.Statement]]
-    ] = GreedyMergeGates(CanMerge(same_id_checker)),
-):
-    topological_groups = get_topological_groups(dag)
-
-    merge_groups = []
-    group_numbers = {}
-
-    for topological_group in topological_groups:
-        stmts = [dag.stmts[node_id] for node_id in topological_group]
-        gate_groups = merge_gates(stmts)
-        for group in gate_groups:
-            if len(group) == 1:
-                continue
-
-            for gate in group:
-                group_numbers[gate] = len(merge_groups)
-
-            merge_groups.append(group)
---
+++ src/bloqade/qasm2/rewrites/uop_to_parallel.py
@@ -0,0 +1,253 @@
+from typing import Dict, List, Tuple, Callable, Iterable
+from dataclasses import field, dataclass
+
+from kirin import ir
+from kirin.rewrite import abc, result
+from kirin.dialects import ilist
+from bloqade.analysis import address
+from bloqade.qasm2.dialects import uop, parallel
+from bloqade.analysis.schedule import StmtDag
+
+
+def same_id_checker(ssa1: ir.SSAValue, ssa2: ir.SSAValue):
+    return ssa1 is ssa2
+
+
+class MergeResults:
+    def __init__(self, merge_groups, group_numbers):
+        self.merge_groups = merge_groups
+        self.group_numbers = group_numbers
+
+
+@dataclass
+class GreedyParallelGrouping:
+    ssa_value_checker: Callable[[ir.SSAValue, ir.SSAValue], bool] = field(
+        default=same_id_checker
+    )
+
+    def check_equiv_args(
+        self,
+        args1: Iterable[ir.SSAValue],
+        args2: Iterable[ir.SSAValue],
+    ):
+        try:
+            return all(
+                self.ssa_value_checker(ssa1, ssa2)
+                for ssa1, ssa2 in zip(args1, args2, strict=True)
+            )
+        except ValueError:
+            return False
+
+    def policy(self, stmt1: ir.Statement, stmt2: ir.Statement):
+        from bloqade.qasm2.dialects import uop, parallel
+
+        match stmt1, stmt2:
+            case (
+                (parallel.UGate(), parallel.UGate())
+                | (parallel.UGate(), uop.UGate())
+                | (uop.UGate(), parallel.UGate())
+                | (uop.UGate(), parallel.UGate())
+                | (uop.UGate(), parallel.UGate())
+                | (parallel.RZ(), parallel.RZ())
+            ):
+                return self.check_equiv_args(stmt1.args[1:], stmt2.args[1:])
+            case (
+                (parallel.CZ(), parallel.CZ())
+                | (parallel.CZ, uop.CZ)
+                | (uop.CZ, parallel.CZ)
+            ):
+                return True
+
+            case _:
+                return False
+
+    def merge_gates(self, gate_stmts: Iterable[ir.Statement]):
+        groups = []
+
+        for gate in gate_stmts:
+            grouped = False
+            for group in groups:
+                if any(self.policy(gate, group_gate) for group_gate in group):
+                    group.append(gate)
+                    grouped = True
+                    break
+
+            if not grouped:
+                groups.append([gate])
+
+        return groups
+
+    def topological_groups(self, dag: StmtDag):
+        inc_edges = {k: set(v) for k, v in dag.inc_edges.items()}
+        # worse case is a linear dag,
+        # so we can use len(dag.stmts) as the limit
+        for _ in range(len(dag.stmts)):
+            if len(inc_edges) == 0:
+                break
+            # get edges with no dependencies
+            group = [
+                node_id for node_id, inc_edges in inc_edges.items() if not inc_edges
+            ]
+            # remove nodes in group from inc_edges
+            for n in group:
+                inc_edges.pop(n)
+                for m in dag.out_edges[n]:
+                    inc_edges[m].remove(n)
+
+            yield group
+
+        if inc_edges:
+            raise ValueError("Cyclic dependency detected")
+
+    def __call__(
+        self,
+        dag: StmtDag,
+    ):
+        merge_groups: List[List[ir.Statement]] = []
+        group_numbers: Dict[ir.Statement, int] = {}
+
+        for topological_group in self.topological_groups(dag):
+            if len(topological_group) == 1:
+                continue
+
+            stmts = map(dag.stmts.__getitem__, topological_group)
+            gate_groups = self.merge_gates(stmts)
+
+            for group in gate_groups:
+                if len(group) == 1:
+                    continue
+
+                for gate in group:
+                    group_numbers[gate] = len(merge_groups)
+
+                merge_groups.append(group)
+
+        return MergeResults(merge_groups, group_numbers)
+
+
+@dataclass
+class UOpToParallel(abc.RewriteRule):
+    address_analysis: Dict[ir.SSAValue, address.Address]
+    dags: Dict[ir.Block, StmtDag]
+    parallel_grouping: Callable[[StmtDag], MergeResults] = field(init=False)
+
+    merge_groups: List[List[ir.Statement]] = field(init=False)
+    group_numbers: Dict[ir.Statement, int] = field(init=False)
+
+    def __post_init__(self):
+        self.parallel_grouping = GreedyParallelGrouping()
+
+    def rewrite_Block(self, block: ir.Block):
+        merge_results = self.parallel_grouping(self.dags[block])
+        self.merge_groups = merge_results.merge_groups
+        self.group_numbers = merge_results.group_numbers
+
+        return result.RewriteResult()
+
+    def rewrite_Statement(self, node: ir.Statement) -> result.RewriteResult:
+        if node not in self.group_numbers:
+            return result.RewriteResult()
+
+        group_number = self.group_numbers[node]
+        group = self.merge_groups[group_number]
+
+        if node is group[-1]:
+            method = getattr(self, f"rewrite_group_{node.name}")
+            method(group)
+
+        node.delete()
+
+        return result.RewriteResult(has_done_something=True)
+
+    def merge_qubits(self, qargs: List[ir.SSAValue]) -> Tuple[ir.SSAValue, ...]:
+
+        qubits = []
+
+        for qarg in qargs:
+            addr = self.address_analysis[qarg]
+
+            if isinstance(addr, address.AddressQubit):
+                qubits.append(qarg)
+
+            elif isinstance(addr, address.AddressTuple):
+                assert isinstance(qarg, ir.ResultValue)
+                assert isinstance(qarg.stmt, ilist.New)
+                qubits.extend(qarg.stmt.values)
+
+        return tuple(qubits)
+
+    def rewrite_group_cz(self, group: List[ir.Statement]):
+        ctrls = []
+        qargs = []
+
+        for stmt in group:
+            if isinstance(stmt, uop.CZ):
+                ctrls.append(stmt.ctrl)
+                qargs.append(stmt.qarg)
+            elif isinstance(stmt, parallel.CZ):
+                ctrls.append(stmt.ctrls)
+                qargs.append(stmt.qargs)
+            else:
+                raise RuntimeError(f"Unexpected statement {stmt}")
+
+        ctrls_values = self.merge_qubits(ctrls)
+        qargs_values = self.merge_qubits(qargs)
+
+        last_stmt = group[-1]
+        new_ctrls = ilist.New(values=ctrls_values)
+        new_qargs = ilist.New(values=qargs_values)
+        new_gate = parallel.CZ(ctrls=new_ctrls.result, qargs=new_qargs.result)
+
+        new_gate.insert_after(last_stmt)
+        new_qargs.insert_after(new_gate)
+        new_ctrls.insert_after(new_qargs)
+
+    def rewrite_group_u(self, group: List[ir.Statement]):
+        qargs = []
+
+        for stmt in group:
+            if isinstance(stmt, uop.UGate):
+                qargs.append(stmt.qarg)
+            elif isinstance(stmt, parallel.UGate):
+                qargs.append(stmt.qargs)
+            else:
+                raise RuntimeError(f"Unexpected statement {stmt}")
+
+        last_stmt = group[-1]
+        assert isinstance(last_stmt, (uop.UGate, parallel.UGate))
+
+        qargs_values = self.merge_qubits(qargs)
+        new_qargs = ilist.New(values=qargs_values)
+        new_gate = parallel.UGate(
+            qargs=new_qargs.result,
+            theta=last_stmt.theta,
+            phi=last_stmt.phi,
+            lam=last_stmt.lam,
+        )
+
+        new_gate.insert_after(last_stmt)
+        new_qargs.insert_after(new_gate)
+
+    def rewrite_group_rz(self, group: List[ir.Statement]):
+        qargs = []
+
+        for stmt in group:
+            if isinstance(stmt, uop.RZ):
+                qargs.append(stmt.qarg)
+            elif isinstance(stmt, parallel.RZ):
+                qargs.append(stmt.qargs)
+            else:
+                raise RuntimeError(f"Unexpected statement {stmt}")
+
+        last_stmt = group[-1]
+        assert isinstance(last_stmt, (uop.RZ, parallel.RZ))
+
+        qargs_values = self.merge_qubits(qargs)
+        new_qargs = ilist.New(values=qargs_values)
+        new_gate = parallel.RZ(
+            qargs=new_qargs.result,
+            theta=last_stmt.theta,
+        )
+
+        new_gate.insert_after(last_stmt)
+        new_qargs.insert_after(new_gate)
