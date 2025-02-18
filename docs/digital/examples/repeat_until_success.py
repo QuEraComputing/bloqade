f5cdfb825ad3c6141cea6283a11c21f9d46862bd
Jonathan Wurtz <jwurtz@quera.com>
2025-02-18 11:10:46 -0500

    Add documentation to RUS example

--- docs/digital/examples/repeat_until_success.py
+++ docs/digital/examples/repeat_until_success.py
@@ -1,15 +1,70 @@
+# %% [markdown]
+# # Repeat Until Success with STAR Gadget
+# In this example, we will demonstrate a near-term fault tolerant gadget
+# which is a repeat-until-success protocol to implement a Z phase gate
+# using a resource state (similar to a T state), Pauli gates, and feed-forward measurement.
+#
+# For more information, see https://journals.aps.org/prxquantum/pdf/10.1103/PRXQuantum.5.010337,
+# especially Fig. 7.
+
 from bloqade import qasm2
 
+#%% [markdown]
+# This example highlights a few interesting capabilities of having a full kernel structure with
+# runtime control flow. One example is the ability to dynamically allocate qubits, possibly
+# based on previous run-time measurement outcomes.
+#
+# In this case, we prepare a resource state, which is a generalization of the T state with
+# an arbitrary Z rotation $|0\rangle + e^{i\theta}|1\rangle$.
 
 @qasm2.extended
 def prep_resource_state(theta: float) -> qasm2.types.QReg:
     qreg = qasm2.qreg(1)
+    qasm2.h(qreg[0])
     qasm2.rz(qreg[0], theta)
     return qreg
 
+#%% [markdown]
+# Using this resource state, we can teleport the Z phase gate to a target qubit using
+# only Clifford gates, which are much easier to implement fault-tolerantly.
+# This is implemented by first applying a CNOT gate controlled by the resource state
+# on the target qubit, then measuring the target qubit in the computational basis.
+# If the measurement outcome is 1 (which occurs with 50% probability), the gadget
+# executed a Z(theta) gate on the target qubit and teleported it
+# to the new resource state.
+#
+# However, if the measurement outcome is 0 (which occurs with 50% probability),
+# we apply an X gate, and the gadget executed a Z(-theta) gate on the target qubit.
+# In order to correct this gate, we must apply a Z(+2*theta) gate on the new target state.
+# Of course, we can apply this Z(+2*theta) gate by applying the same gadget with twice
+# the angle, and repeat until we get the correct outcome.
+
+#%% [markdown]
+# The simplest way to implement the gadget is to simply post-select the correct measurement outcome
+# using an assert statement. This is straightforward, but comes with an exponential overhead in the
+# number of resource states: there is a 50% chance of success at each step, so there is only a
+# $2^{-n}$ chance of success after $n$ Z phase gates.
 
 @qasm2.extended
-def star_gadget_recursive(target: qasm2.types.Qubit, theta: float) -> qasm2.types.QReg:
+def z_phase_gate_postselect(target: qasm2.types.Qubit, theta: float) -> qasm2.types.QReg:
+    ancilla = prep_resource_state(theta)
+    qasm2.cx(ancilla[0], target)
+    creg = qasm2.creg(1)
+    qasm2.measure(target, creg[0])
+    if creg[0] == 1:
+        return ancilla
+    else:
+        assert "Did not converge"
+        return ancilla
+        
+#%% [markdown]
+# To (deterministically) implement the gate, we can recursively apply the gadget.
+# Observe that, while it is efficient to represent this as a composition of kernels,
+# there is no equivalent representation as a circuit, as the number of resource qubits and
+# total number of gates is not known until runtime.
+
+@qasm2.extended
+def z_phase_gate_recursive(target: qasm2.types.Qubit, theta: float) -> qasm2.types.QReg:
     """
     https://journals.aps.org/prxquantum/pdf/10.1103/PRXQuantum.5.010337 Fig. 7
     """
@@ -22,12 +77,17 @@ def star_gadget_recursive(target: qasm2.types.Qubit, theta: float) -> qasm2.type
         return ancilla
     else:
         qasm2.x(ancilla[0])
-        return star_gadget_recursive(ancilla[0], 2 * theta)
+        return z_phase_gate_recursive(ancilla[0], 2 * theta)
 
+#%% [markdown]
+# An alternative representation uses structural control flow (for and while loops) to
+# implement the same gate. If the number of repeats is fixed, this can be represented
+# as a static circuit, though it would require a large number of resource qubits and
+# may still fail with a small probability $2^{-attempts}$.
 
 @qasm2.extended
-def star_gadget_loop(
-    target: qasm2.types.Qubit, theta: float, attempts: int = 100
+def z_phase_gate_loop(
+    target: qasm2.types.Qubit, theta: float, attempts: int = 10
 ) -> qasm2.types.QReg:
     """
     https://journals.aps.org/prxquantum/pdf/10.1103/PRXQuantum.5.010337 Fig. 7
@@ -49,3 +109,36 @@ def star_gadget_loop(
     assert converged, "Loop did not converge"
 
     return ancilla
+
+#%% [markdown]
+# Lets unwrap the postselection and loop versions of the gadget to see the qasm circuit.
+
+from bloqade.qasm2.emit import QASM2  # noqa: E402
+from bloqade.qasm2.parse import pprint  # noqa: E402
+
+
+
+# qasm2 does not support parameterized circuits, so the entry point must be a function
+# of zero arguments. We can use a closure to pass the parameters from outside the function.
+theta = 0.1
+@qasm2.extended
+def main():
+    target = qasm2.qreg(1)
+    return z_phase_gate_postselect(target[0], theta)
+
+target = QASM2()
+ast = target.emit(main)
+pprint(ast)
+
+#%% [markdown]
+# And now the loop version, which first needs to be unwrapped:
+
+theta = 0.1
+@qasm2.extended
+def main2():
+    target = qasm2.qreg(1)
+    return z_phase_gate_loop(target[0], theta)
+
+target = QASM2()
+ast = target.emit(main2)
+pprint(ast)
