86a0896c759a30487571f739ebdca164de3b0f99
Phillip Weinberg <weinbe58@gmail.com>
2025-02-22 16:00:09 -0500

    refactor native noise to be more parallel

--- src/bloqade/noise/native/_wrappers.py
+++ src/bloqade/noise/native/_wrappers.py
@@ -1,26 +1,29 @@
 from bloqade.noise import native
+from kirin.dialects import ilist
 from kirin.lowering import wraps
 from bloqade.qasm2.types import Qubit
 
 
 @wraps(native.AtomLossChannel)
-def atom_loss_channel(prob: float, qarg: Qubit) -> None: ...
+def atom_loss_channel(prob: float, qargs: ilist.IList[Qubit] | list) -> None: ...
 
 
 @wraps(native.PauliChannel)
-def pauli_channel(px: float, py: float, pz: float, qarg: Qubit) -> None: ...
+def pauli_channel(
+    px: float, py: float, pz: float, qargs: ilist.IList[Qubit] | list
+) -> None: ...
 
 
 @wraps(native.CZPauliChannel)
 def cz_pauli_channel(
+    ctrls: ilist.IList[Qubit] | list,
+    qarg2: ilist.IList[Qubit] | list,
+    *,
     px_1: float,
     py_1: float,
     pz_1: float,
     px_2: float,
     py_2: float,
     pz_2: float,
-    qarg1: Qubit,
-    qarg2: Qubit,
-    *,
     paired: bool,
 ) -> None: ...
