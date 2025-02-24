df800efd8a0ef14ab772c2ee1101e47ba7419fcf
Phillip Weinberg <weinbe58@gmail.com>
2025-02-23 23:29:10 -0500

    clean up tests.

--- test/qasm2/emit/test_qasm2_emit.py
+++ test/qasm2/emit/test_qasm2_emit.py
@@ -10,18 +10,21 @@ def test_global():
         qreg1 = qasm2.qreg(3)
         qasm2.glob.u(theta=0.1, phi=0.2, lam=0.3, registers=[qreg, qreg1])
 
-    glob_u.print()
-
     target = qasm2.emit.QASM2(
         main_target=qasm2.main.add(qasm2.dialects.glob).add(ilist),
         gate_target=qasm2.gate.add(qasm2.dialects.glob).add(ilist),
         custom_gate=True,
     )
-    ast = target.emit(glob_u)
-    qasm2.parse.pprint(ast)
-
-
-test_global()
+    qasm2_str = target.emit_str(glob_u)
+    assert (
+        qasm2_str
+        == """KIRIN {func,lowering.call,lowering.func,py.ilist,qasm2.core,qasm2.expr,qasm2.glob,qasm2.indexing,qasm2.uop,scf};
+include "qelib1.inc";
+qreg qreg[3];
+qreg qreg1[3];
+glob.U(0.1, 0.2, 0.3) {qreg, qreg1}
+"""
+    )
 
 
 def test_para():
@@ -38,8 +41,15 @@ def test_para():
         gate_target=qasm2.gate.add(qasm2.dialects.parallel).add(ilist),
         custom_gate=True,
     )
-    ast = target.emit(para_u)
-    qasm2.parse.pprint(ast)
-
-
-test_para()
+    qasm2_str = target.emit_str(para_u)
+    assert (
+        qasm2_str
+        == """KIRIN {func,lowering.call,lowering.func,py.ilist,qasm2.core,qasm2.expr,qasm2.indexing,qasm2.parallel,qasm2.uop,scf};
+include "qelib1.inc";
+qreg qreg[3];
+parallel.U(0.1, 0.2, 0.3) {
+  qreg[0];
+  qreg[1];
+}
+"""
+    )
