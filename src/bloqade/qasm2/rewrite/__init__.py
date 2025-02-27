63db50fe51a5acc20f2a751e8167e450a018ae7f
Phillip Weinberg <weinbe58@gmail.com>
2025-02-26 20:57:49 -0500

    split up module

--- src/bloqade/qasm2/rewrite/__init__.py
+++ src/bloqade/qasm2/rewrite/__init__.py
@@ -1,4 +1,5 @@
 from .global_to_uop import GlobalToUOpRule as GlobalToUOpRule
+from .heuristic_noise import NoiseRewriteRule as NoiseRewriteRule
 from .parallel_to_uop import ParallelToUOpRule as ParallelToUOpRule
 from .uop_to_parallel import (
     MergePolicyABC as MergePolicyABC,
