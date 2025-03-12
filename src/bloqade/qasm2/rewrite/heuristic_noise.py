82686148157c956a1c0b2efe03daaca5ba882dbc
Phillip Weinberg <weinbe58@gmail.com>
2025-03-12 10:59:15 -0400

    Allow gate noise to be inserted if Address analysis fails for CZ gate

--- src/bloqade/qasm2/rewrite/heuristic_noise.py
+++ src/bloqade/qasm2/rewrite/heuristic_noise.py
@@ -181,13 +181,24 @@ class NoiseRewriteRule(result_abc.RewriteRule):
         ]
 
     def rewrite_cz_gate(self, node: uop.CZ):
+
+        (ctrls := ilist.New([node.ctrl])).insert_before(node)
+        (qargs := ilist.New([node.qarg])).insert_before(node)
+        gate_noise_nodes = self.cz_gate_noise(ctrls.result, qargs.result)
+
+        has_done_something = False
+
+        for new_node in gate_noise_nodes:
+            has_done_something = True
+            new_node.insert_before(node)
+
         qarg_addr = self.address_analysis[node.qarg]
         ctrl_addr = self.address_analysis[node.ctrl]
 
         if not isinstance(qarg_addr, address.AddressQubit) or not isinstance(
             ctrl_addr, address.AddressQubit
         ):
-            return result.RewriteResult()
+            return result.RewriteResult(has_done_something=has_done_something)
 
         other_qubits = sorted(
             set(self.qubit_ssa_value.keys()) - {ctrl_addr.data, qarg_addr.data}
@@ -195,45 +206,43 @@ class NoiseRewriteRule(result_abc.RewriteRule):
         errors = self.noise_model.parallel_cz_errors(
             [ctrl_addr.data], [qarg_addr.data], other_qubits
         )
-        (ctrls := ilist.New([self.qubit_ssa_value[ctrl_addr.data]])).insert_before(node)
-        (qargs := ilist.New([self.qubit_ssa_value[qarg_addr.data]])).insert_before(node)
+
         move_noise_nodes = self.move_noise_stmts(errors)
-        gate_noise_nodes = self.cz_gate_noise(ctrls.result, qargs.result)
 
         for new_node in move_noise_nodes:
-            new_node.insert_before(node)
-
-        for new_node in gate_noise_nodes:
             new_node.insert_before(node)
 
         return result.RewriteResult(has_done_something=True)
 
     def rewrite_parallel_cz_gate(self, node: parallel.CZ):
+
+        gate_noise_nodes = self.cz_gate_noise(node.ctrls, node.qargs)
+
+        has_done_something = False
+        for new_node in gate_noise_nodes:
+            new_node.insert_before(node)
+            has_done_something = True
+
         ctrls = self.address_analysis[node.ctrls]
         qargs = self.address_analysis[node.qargs]
 
         if not isinstance(ctrls, address.AddressTuple) or not isinstance(
             qargs, address.AddressTuple
         ):
-            return result.RewriteResult()
+            return result.RewriteResult(has_done_something=has_done_something)
 
         if not all(
             isinstance(addr, address.AddressQubit) for addr in ctrls.data
         ) or not all(isinstance(addr, address.AddressQubit) for addr in qargs.data):
-            return result.RewriteResult()
+            return result.RewriteResult(has_done_something=has_done_something)
 
         ctrl_qubits = list(map(lambda addr: addr.data, ctrls.data))
         qarg_qubits = list(map(lambda addr: addr.data, qargs.data))
         rest = sorted(set(self.qubit_ssa_value.keys()) - set(ctrl_qubits + qarg_qubits))
         errors = self.noise_model.parallel_cz_errors(ctrl_qubits, qarg_qubits, rest)
-
         move_noise_nodes = self.move_noise_stmts(errors)
-        gate_noise_nodes = self.cz_gate_noise(node.ctrls, node.qargs)
 
         for new_node in move_noise_nodes:
-            new_node.insert_before(node)
-
-        for new_node in gate_noise_nodes:
             new_node.insert_before(node)
 
         return result.RewriteResult(has_done_something=True)
