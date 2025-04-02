a47517740b9b14376a835cf2816f27a5843360e8
John Long <johnzl@protonmail.ch>
2025-04-02 13:45:22 -0400

    got help from Phillip, realized I need to fold + do typeinference to set the proper attributes

--- squin_playground.py
+++ squin_playground.py
@@ -1,13 +1,32 @@
+from kirin import ir, passes
 from bloqade import squin
 from kirin.ir import dialect_group
-from kirin.prelude import basic_no_opt
+from kirin.prelude import basic
 from bloqade.analysis import address
 
 
-@dialect_group(basic_no_opt.add(squin.wire.dialect).add(squin.qubit.dialect))
+@dialect_group(basic.add(squin.wire).add(squin.qubit))
 def squin_dialect(self):
-    # usually plug in Fold pass, hold off for now
-    return
+    # Const prop analysis runs first, then fold pass takes
+    # ConstantFold puts in the type hints! Need that for
+    fold_pass = passes.Fold(self)
+    typeinfer_pass = passes.TypeInfer(self)
+
+    def run_pass(
+        method: ir.Method,
+        *,
+        fold: bool = True,
+    ):
+        method.verify()
+        # TODO make special Function rewrite
+
+        if fold:
+            fold_pass(method)
+
+        typeinfer_pass(method)
+        method.code.typecheck()
+
+    return run_pass
 
 
 @squin_dialect
@@ -19,7 +38,6 @@ def main():
     return qubits
 
 
-main.print()
 frame, _ = address.AddressAnalysis(main.dialects).run_analysis(main)
 
 print(frame)
